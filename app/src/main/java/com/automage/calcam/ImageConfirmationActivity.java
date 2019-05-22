ba5dc20b7bc0a97897e8228f5113ae59bf37cd28
Pranav <pranav92000@gmail.com>
2019-05-23 02:24:21 +0400

    Replaced FAB with AppBar - Fullscreen scrollview in ImageConfirmationActivity now - Confirmation action now located in appbar

--- app/src/main/java/com/automage/calcam/ImageConfirmationActivity.java
+++ app/src/main/java/com/automage/calcam/ImageConfirmationActivity.java
@@ -6,13 +6,17 @@ import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -26,7 +30,6 @@ import java.util.List;
 
 public class ImageConfirmationActivity extends AppCompatActivity {
 
-    private FloatingActionButton fab;
     private ImageView displayedImageView;
     private TextView parsedTextView;
 
@@ -37,16 +40,15 @@ public class ImageConfirmationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_confirmation);
 
+        Toolbar toolbar = findViewById(R.id.image_confirmation_toolbar);
+        setSupportActionBar(toolbar);
+
+        //Enables Up (Back) button
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
 
         displayedImageView = findViewById(R.id.displayed_image);
         parsedTextView = findViewById(R.id.parsed_text);
-        fab = findViewById(R.id.confirmation_fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                launchEditEventActivity();
-            }
-        });
 
         Intent data = getIntent();
         Uri imageUri = Uri.parse(data.getStringExtra(MainActivity.EXTRA_IMAGE_URI));
@@ -115,6 +117,23 @@ public class ImageConfirmationActivity extends AppCompatActivity {
             }
         }
         parsedTextView.setText(temp);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.Z
+        getMenuInflater().inflate(R.menu.image_confirmation_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.image_confirmation_confirm_action) {
+            launchEditEventActivity();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 
     private void showToast(String message) {
